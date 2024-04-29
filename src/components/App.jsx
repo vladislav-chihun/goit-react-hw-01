a3da8557cb3e37ec7ab600948090d052af209f8d
vladislav-chihun <vladikkosmo@gmail.com>
2024-04-29 15:04:52 +0200

    f

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-
-
-export default App
+import Profile from "./Profile/Profile"
+export default function App() {
+  return (
+    <>
+    
+    </>)
+}
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,28 +1,28 @@
-export default function Profile() {
-return <div>
-    <div>
-        <img
-            src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-            alt="User avatar"
-        />
-        <p>Petra Marica</p>
-        <p>@pmarica</p>
-        <p>Salvador, Brasil</p>
-    </div>
+export default function Profile(props) {
+    return <div>
+        <div>
+            <img
+                src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+                alt="User avatar"
+            />
+            <p>Petra Marica</p>
+            <p>@pmarica</p>
+            <p>Salvador, Brasil</p>
+        </div>
 
-    <ul>
-        <li>
-      <span>Followers</span>
-      <span>1000</span>
-        </li>
-        <li>
-      <span>Views</span>
-      <span>2000</span>
-        </li>
-        <li>
-      <span>Likes</span>
-      <span>3000</span>
-        </li>
-    </ul>
-</div>
+        <ul>
+            <li>
+                <span>Followers</span>
+                <span>1000</span>
+            </li>
+            <li>
+                <span>Views</span>
+                <span>2000</span>
+            </li>
+            <li>
+                <span>Likes</span>
+                <span>3000</span>
+            </li>
+        </ul>
+    </div>;
 }
