929f7d51e9178e6bc9cdf338e0268dcd82310e76
vladislav-chihun <vladikkosmo@gmail.com>
2024-04-29 23:54:40 +0200

    f

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,13 +1,13 @@
 import css from "./Profile.module.css"
 export default function Profile({ name, tag, location, image, stats: { followers, views, likes } }) {
-    return <div>
-        <div>
+    return <div className={css.container}>
+        <div className={css.profile}>
             <img
                 src={image}
                 alt="User avatar"
             />
-            <p>{name}</p>
-            <p>{tag}</p>
+            <p className={css.name}>{name}</p>
+            <p>@{tag}</p>
             <p>{location}</p>
         </div>
 
